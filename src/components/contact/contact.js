6a728009cb37792b2ae4c134efb2ff64794f7896
Mearat Hou <mearatjames@gmail.com>
2019-02-07 00:11:54 -0800

    Added reveal effect

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Container, Button, TextArea, Form } from 'semantic-ui-react'
+import { Container, Button, TextArea, Form, Icon, Header } from 'semantic-ui-react'
+import Slide from 'react-reveal/Slide';
 import './contact.css'
 
 class Contact extends Component {
@@ -9,15 +10,21 @@ render() {
     return (
         <div id='contact'>
           <Container className='contact'>
+            <Header textAlign='center' as='h2' icon>
+                <Slide left>
+                    <Icon name='paper plane' />
+                </Slide>
+                Contact
+            </Header>
             <Form name="contact" action='#' method="post" data-netlify="true">
                 <Form.Field>
                     <input type="hidden" name="form-name" value="contact" />
                 </Form.Field>
-                <Form.Field>
+                <Form.Field required>
                     <label>Name</label>
-                    <input name='name' type='text' placeholder='Name' />
+                    <input name='name' type='text' required placeholder='Name' />
                 </Form.Field>
-                <Form.Field>
+                <Form.Field required>
                     <label>Email</label>
                     <input name='email' type='email' placeholder='Email' />
                 </Form.Field>
@@ -26,6 +33,7 @@ render() {
                     control={TextArea}
                     label='Message'
                     placeholder='Message'
+                    required
                     />
                 <Form.Field>
                     <div data-netlify-recaptcha="true"></div>
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,46 +1,44 @@
 import React, { Component } from 'react'
 import { Menu, Container } from 'semantic-ui-react'
+import Scrollspy from 'react-scrollspy'
+import Fade from 'react-reveal/Fade';
 import './nav.css'
 
 
 class NavMenu extends Component {
-  state = { activeItem: 'about' }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
-    const { activeItem } = this.state
 
     return (
         <div className='shadow'>
             <Menu className='menu' style={{background: 'rgba(255, 255, 255, 0.9)'}} text>
                 <Container className='navBar'>
-                        <Menu.Item
+                    <Menu.Item
                         href='#showcase'
                         className='logo'
                         >
+                        <Fade>
                             <img style={{width: '120px'}} src='./images/logo.png' alt='Logo'/>
-                        </Menu.Item>
-                    <div className='menuItems'>
-                        <Menu.Item
-                        name='about'
-                        active={activeItem === 'about'}
-                        href='#about'
-                        onClick={this.handleItemClick}
-                        />
+                        </Fade>
+                    </Menu.Item>
+                    <Scrollspy className='menuItems' offset={-200} items={ ['showcase', 'portfolio', 'about', 'contact'] } currentClassName="active">
+                        <a href='#showcase' style={{display:'none'}}>Showcase</a>
                         <Menu.Item
                         name='work'
                         href='#portfolio'
-                        active={activeItem === 'work'}
+                        onClick={this.handleItemClick}
+                        />
+                        <Menu.Item
+                        name='about'
+                        href='#about'
                         onClick={this.handleItemClick}
                         />
                         <Menu.Item
                         name='contact'
                         href='#contact'
-                        active={activeItem === 'contact'}
                         onClick={this.handleItemClick}
                         />
-                    </div>
+                    </Scrollspy>
                 </Container>
             </Menu>
         </div>
