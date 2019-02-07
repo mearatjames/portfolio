1ecb6c11109c224dc90673beacb4affb2f3f2181
Mearat Hou <mearatjames@gmail.com>
2019-02-07 15:22:35 -0800

    Added Type Writer Effect

--- src/components/showcase/showcase.js
+++ src/components/showcase/showcase.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './showcase.css'
+import Typewriter from 'typewriter-effect';
 
 class Showcase extends Component {
 
@@ -10,7 +11,13 @@ render() {
           <div className='showcase'>
             <div style={{textAlign: 'center', zIndex: 10}}>
               <p>Mearat Hou</p>
-              <p>Javascript Developer</p>
+              <Typewriter
+                options={{
+                  strings: ['Javascript Developer', 'React Developer', 'Web Developer'],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
             </div>
             <div className='circle'></div>
             <div className='square'></div>
