ab00ee3f3458a2653a639048ce1a871e3eb9f535
Mearat Hou <mearatjames@gmail.com>
2019-02-06 17:27:26 -0800

    Netlify form submission test

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -9,25 +9,26 @@ render() {
     return (
         <div id='contact'>
           <Container className='contact'>
-            <Form name="contact" method="post" data-netlify="true">
-                <input type="hidden" name="form-name" value="contact" />
+            <Form name="contact" action='#' method="post" data-netlify="true">
+                <Form.Field>
+                    <input type="hidden" name="form-name" value="contact" />
+                </Form.Field>
                 <Form.Field>
                     <label>Name</label>
-                    <input name='name' placeholder='Name' />
+                    <input name='name' type='text' placeholder='Name' />
                 </Form.Field>
                 <Form.Field>
                     <label>Email</label>
                     <input name='email' type='email' placeholder='Email' />
                 </Form.Field>
                 <Form.Field
-                    id='form-textarea-control-opinion'
                     name='message'
                     control={TextArea}
                     label='Message'
                     placeholder='Message'
                     />
                 <Form.Field>
-                <div data-netlify-recaptcha="true"></div>
+                    <div data-netlify-recaptcha="true"></div>
                 </Form.Field>
                 <Button type='submit'>Submit</Button>
             </Form>
