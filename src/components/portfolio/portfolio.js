522832d3d6caa1a500a0bf82a33810ac8e5e141f
Mearat Hou <mearatjames@gmail.com>
2019-02-11 12:37:29 -0800

    Added Media Queries for work section

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -65,6 +65,54 @@ render() {
                     </Card.Content>
                 </Card>
             </div>
+            <div> 
+                <Card className='cards' fluid>
+                    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                    <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <a href='./'>
+                            <Icon name='user' />
+                            10 Friends
+                        </a>
+                    </Card.Content>
+                </Card>
+            </div>
+            <div> 
+                <Card className='cards' fluid>
+                    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                    <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <a href='./'>
+                            <Icon name='user' />
+                            10 Friends
+                        </a>
+                    </Card.Content>
+                </Card>
+            </div>
+            <div> 
+                <Card className='cards' fluid>
+                    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                    <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <a href='./'>
+                            <Icon name='user' />
+                            10 Friends
+                        </a>
+                    </Card.Content>
+                </Card>
+            </div>
          </Container>
         </div>
     </div>
