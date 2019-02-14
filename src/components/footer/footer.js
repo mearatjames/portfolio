70fd40cd0b87a15d4c14091e606040c3c9837b94
Mearat Hou <mearatjames@gmail.com>
2019-02-14 11:12:15 -0800

    added work thumbnails and links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -13,22 +13,22 @@ class Footer extends Component {
                     <h3>Connect</h3>
                 </div>
                 <div className='list3'>
-                    <a href='https://github.com/mearatjames'><h3><Icon name='github' /> Github</h3></a> 
-                    <a href='https://www.linkedin.com/in/mearat-hou-013a93164/'><h3><Icon name='linkedin' /> LinkedIn</h3></a> 
-                    <a href="mailto:mearatjames@gmail.com"><h3><Icon name='envelope' /> Email</h3></a> 
+                    <a target='blank' href='https://github.com/mearatjames'><h3><Icon name='github' /> Github</h3></a> 
+                    <a target='blank' href='https://www.linkedin.com/in/mearat-hou-013a93164/'><h3><Icon name='linkedin' /> LinkedIn</h3></a> 
+                    <a target='blank' href="mailto:mearatjames@gmail.com"><h3><Icon name='envelope' /> Email</h3></a> 
                 </div>
                 <div className='project'>
                     <h3>Work</h3>
                 </div>
                 <div className='list1'>
-                    <a href='https://github.com/mearatjames'><h3>Deez Nutrients</h3></a> 
-                    <a href='https://www.linkedin.com/in/mearat-hou-013a93164/'><h3>NYT Scraping</h3></a> 
-                    <a href="mailto:mearatjames@gmail.com"><h3>Googlebooks</h3></a> 
+                    <a target='blank' href='https://github.com/mearatjames/Deez-Nutrients'><h3>Deez Nutrients</h3></a> 
+                    <a target='blank' href='https://github.com/mearatjames/nytScrapping'><h3>NYT Scraping</h3></a> 
+                    <a target='blank' href="https://github.com/mearatjames/googlebooks"><h3>Googlebooks</h3></a> 
                 </div>
                 <div className='list2'>
-                    <a href='https://github.com/mearatjames'><h3>Web Accounting</h3></a> 
-                    <a href='https://www.linkedin.com/in/mearat-hou-013a93164/'><h3>Deez Polls</h3></a> 
-                    <a href="mailto:mearatjames@gmail.com"><h3>Trivia Game</h3></a> 
+                    <a target='blank' href='https://github.com/stevenguy/bc-project-3'><h3>Web Accounting</h3></a> 
+                    <a target='blank' href='https://github.com/bunrithbuth/bc-project-2'><h3>Deez Polls</h3></a> 
+                    <a target='blank' href="https://github.com/mearatjames/TriviaGame/tree/dev"><h3>Trivia Game</h3></a> 
                 </div>
                 <div className='upArrow'>
                     <Popup
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -19,96 +19,120 @@ render() {
             </Header>
             <div> 
                 <Card className='cards' fluid>
-                    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                    <Image src='./images/nytscraper.webp' />
                     <Card.Content>
-                    <Card.Header>Daniel</Card.Header>
-                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Header>NYT Scraper</Card.Header>
+                    <Card.Meta>Web Scraping Project</Card.Meta>
                     <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
                     </Card.Content>
-                    <Card.Content extra>
-                        <a href='./'>
-                            <Icon name='user' />
-                            10 Friends
+                    <Card.Content className='cardsLink' extra>
+                        <a target='blank' href='https://still-castle-19414.herokuapp.com/'>
+                            <Icon name='globe' />
+                            Demo Link
+                        </a>
+                        <a target='blank' href='https://github.com/mearatjames/nytScrapping'>
+                            <Icon name='code branch' />
+                            Github Link
                         </a>
                     </Card.Content>
                 </Card>
             </div>
             <div> 
                 <Card className='cards' fluid>
-                    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                    <Image src='./images/googlebooks.webp' />
                     <Card.Content>
-                    <Card.Header>Daniel</Card.Header>
+                    <Card.Header>Google Books</Card.Header>
                     <Card.Meta>Joined in 2016</Card.Meta>
                     <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
                     </Card.Content>
-                    <Card.Content extra>
-                        <a href='./'>
-                            <Icon name='user' />
-                            10 Friends
+                    <Card.Content className='cardsLink' extra>
+                        <a target='blank' href='https://secure-reef-41974.herokuapp.com/'>
+                            <Icon name='globe' />
+                            Demo Link
+                        </a>
+                        <a target='blank' href='https://github.com/mearatjames/googlebooks'>
+                            <Icon name='code branch' />
+                            Github Link
                         </a>
                     </Card.Content>
                 </Card>
             </div>
             <div> 
                 <Card className='cards' fluid>
-                    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                    <Image src='./images/deeznutrients.webp' />
                     <Card.Content>
-                    <Card.Header>Daniel</Card.Header>
+                    <Card.Header>Deez Nutrients</Card.Header>
                     <Card.Meta>Joined in 2016</Card.Meta>
                     <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
                     </Card.Content>
-                    <Card.Content extra>
-                        <a href='./'>
-                            <Icon name='user' />
-                            10 Friends
+                    <Card.Content className='cardsLink' extra>
+                        <a target='blank' href='https://vitamind-b6c3c.firebaseapp.com/'>
+                            <Icon name='globe' />
+                            Demo Link
+                        </a>
+                        <a target='blank' href='https://github.com/mearatjames/Deez-Nutrients'>
+                            <Icon name='code branch' />
+                            Github Link
                         </a>
                     </Card.Content>
                 </Card>
             </div>
             <div> 
                 <Card className='cards' fluid>
-                    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                    <Image src='./images/webaccounting.webp' />
                     <Card.Content>
-                    <Card.Header>Daniel</Card.Header>
+                    <Card.Header>Web Accounting</Card.Header>
                     <Card.Meta>Joined in 2016</Card.Meta>
                     <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
                     </Card.Content>
-                    <Card.Content extra>
-                        <a href='./'>
-                            <Icon name='user' />
-                            10 Friends
+                    <Card.Content className='cardsLink' extra>
+                        <a target='blank' href='https://peaceful-anchorage-98434.herokuapp.com/'>
+                            <Icon name='globe' />
+                            Demo Link
+                        </a>
+                        <a target='blank' href='https://github.com/stevenguy/bc-project-3/tree/mearat'>
+                            <Icon name='code branch' />
+                            Github Link
                         </a>
                     </Card.Content>
                 </Card>
             </div>
             <div> 
                 <Card className='cards' fluid>
-                    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                    <Image src='./images/deezpolls.webp' />
                     <Card.Content>
-                    <Card.Header>Daniel</Card.Header>
+                    <Card.Header>Deez Polls</Card.Header>
                     <Card.Meta>Joined in 2016</Card.Meta>
                     <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
                     </Card.Content>
-                    <Card.Content extra>
-                        <a href='./'>
-                            <Icon name='user' />
-                            10 Friends
+                    <Card.Content className='cardsLink' extra>
+                        <a target='blank' href='https://afternoon-crag-56596.herokuapp.com/polls'>
+                            <Icon name='globe' />
+                            Demo Link
+                        </a>
+                        <a target='blank' href='https://github.com/bunrithbuth/bc-project-2/tree/master'>
+                            <Icon name='code branch' />
+                            Github Link
                         </a>
                     </Card.Content>
                 </Card>
             </div>
             <div> 
                 <Card className='cards' fluid>
-                    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                    <Image src='./images/triviagame.webp' />
                     <Card.Content>
-                    <Card.Header>Daniel</Card.Header>
+                    <Card.Header>Trivia Game</Card.Header>
                     <Card.Meta>Joined in 2016</Card.Meta>
                     <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
                     </Card.Content>
-                    <Card.Content extra>
-                        <a href='./'>
-                            <Icon name='user' />
-                            10 Friends
+                    <Card.Content className='cardsLink' extra>
+                        <a target='blank' href='https://mearatjames.github.io/TriviaGame/'>
+                            <Icon name='globe' />
+                            Demo Link
+                        </a>
+                        <a target='blank' href='https://github.com/mearatjames/TriviaGame'>
+                            <Icon name='code branch' />
+                            Github Link
                         </a>
                     </Card.Content>
                 </Card>
