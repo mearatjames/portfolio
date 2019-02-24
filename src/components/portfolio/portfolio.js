1c580be76065cf7cdfd9cf0befc11df215231398
Mearat Hou <mearatjames@gmail.com>
2019-02-24 13:54:36 -0800

    Refactored portfolio component

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -3,7 +3,60 @@ import {Container, Card, Image, Icon, Header} from 'semantic-ui-react'
 import './portfolio.css'
 import Fade from 'react-reveal/Fade';
 
+const portfolioContents = [
+    {
+        header: 'NYT Scraper',
+        images: './images/nytscraper.webp',
+        meta: 'Web Scraping Project',
+        description: 'A MERN stack web application using Cheerio npm package to scrape news off NYT website.',
+        demoLink: 'https://still-castle-19414.herokuapp.com/',
+        githubLink: 'https://github.com/mearatjames/nytScrapping'
+    },
+    {
+        header: 'Google Books',
+        images: './images/googlebooks.webp',
+        meta: 'Book Searching Project',
+        description: 'Another MERN stack web application using google books api to search for books.',
+        demoLink: 'https://secure-reef-41974.herokuapp.com/',
+        githubLink: 'https://github.com/mearatjames/googlebooks'
+    },
+    {
+        header: 'Deez Nutrients',
+        images: './images/deeznutrients.webp',
+        meta: 'Nutrition Search and Track Application',
+        description: 'First project utilising NutritionIx and Google Charts api to search and track nutritional value.',
+        demoLink: 'https://vitamind-b6c3c.firebaseapp.com/',
+        githubLink: 'https://github.com/mearatjames/Deez-Nutrients'
+    },
+    {
+        header: 'ACCTG Web Accounting',
+        images: './images/webaccounting.webp',
+        meta: 'Basic Web Accounting Application',
+        description: 'A simple enterprise accounting/book-keeping web-based application (UCI Boot Camp Group Project).',
+        demoLink: 'https://peaceful-anchorage-98434.herokuapp.com/',
+        githubLink: 'https://github.com/stevenguy/bc-project-3/tree/mearat'
+    },
+    {
+        header: 'Deez Polls',
+        images: './images/deezpolls.webp',
+        meta: 'Customized Polling Web Application',
+        description: '2nd Group Project Express application using Sequelize and deployed via Heroku.',
+        demoLink: 'https://afternoon-crag-56596.herokuapp.com/polls',
+        githubLink: 'https://github.com/bunrithbuth/bc-project-2/tree/master'
+    },
+    {
+        header: 'Trivia Game',
+        images: './images/triviagame.webp',
+        meta: 'Basic Javascript Application',
+        description: 'Javascript functions and methods warm up in a form of U.S citizenship trivia game.',
+        demoLink: 'https://mearatjames.github.io/TriviaGame/',
+        githubLink: 'https://github.com/mearatjames/TriviaGame'
+    }
+]
+
+
 class Portfolio extends Component {
+
 
 render() {
 
@@ -17,126 +70,28 @@ render() {
                 </Fade>
                 Portfolio
             </Header>
-            <div> 
-                <Card className='cards' fluid>
-                    <Image src='./images/nytscraper.webp' />
-                    <Card.Content>
-                    <Card.Header>NYT Scraper</Card.Header>
-                    <Card.Meta>Web Scraping Project</Card.Meta>
-                    <Card.Description>A MERN stack web application using Cheerio npm package to scrape news off NYT website.</Card.Description>
-                    </Card.Content>
-                    <Card.Content className='cardsLink' extra>
-                        <a target='blank' href='https://still-castle-19414.herokuapp.com/'>
-                            <Icon name='globe' />
-                            Demo Link
-                        </a>
-                        <a target='blank' href='https://github.com/mearatjames/nytScrapping'>
-                            <Icon name='code branch' />
-                            Github Link
-                        </a>
-                    </Card.Content>
-                </Card>
-            </div>
-            <div> 
-                <Card className='cards' fluid>
-                    <Image src='./images/googlebooks.webp' />
-                    <Card.Content>
-                    <Card.Header>Google Books</Card.Header>
-                    <Card.Meta>Book Searching Project</Card.Meta>
-                    <Card.Description>Another MERN stack web application using google books api to search for books.</Card.Description>
-                    </Card.Content>
-                    <Card.Content className='cardsLink' extra>
-                        <a target='blank' href='https://secure-reef-41974.herokuapp.com/'>
-                            <Icon name='globe' />
-                            Demo Link
-                        </a>
-                        <a target='blank' href='https://github.com/mearatjames/googlebooks'>
-                            <Icon name='code branch' />
-                            Github Link
-                        </a>
-                    </Card.Content>
-                </Card>
-            </div>
-            <div> 
-                <Card className='cards' fluid>
-                    <Image src='./images/deeznutrients.webp' />
-                    <Card.Content>
-                    <Card.Header>Deez Nutrients</Card.Header>
-                    <Card.Meta>Nutrition Search Application</Card.Meta>
-                    <Card.Description>First project utilising NutritionIx and Google Charts api to search and track nutritional value.</Card.Description>
-                    </Card.Content>
-                    <Card.Content className='cardsLink' extra>
-                        <a target='blank' href='https://vitamind-b6c3c.firebaseapp.com/'>
-                            <Icon name='globe' />
-                            Demo Link
-                        </a>
-                        <a target='blank' href='https://github.com/mearatjames/Deez-Nutrients'>
-                            <Icon name='code branch' />
-                            Github Link
-                        </a>
-                    </Card.Content>
-                </Card>
-            </div>
-            <div> 
-                <Card className='cards' fluid>
-                    <Image src='./images/webaccounting.webp' />
-                    <Card.Content>
-                    <Card.Header>ACCTG Web Accounting</Card.Header>
-                    <Card.Meta>Basic Web Accounting Application</Card.Meta>
-                    <Card.Description>A simple enterprise accounting/book-keeping web-based application (UCI Boot Camp Group Project).</Card.Description>
-                    </Card.Content>
-                    <Card.Content className='cardsLink' extra>
-                        <a target='blank' href='https://peaceful-anchorage-98434.herokuapp.com/'>
-                            <Icon name='globe' />
-                            Demo Link
-                        </a>
-                        <a target='blank' href='https://github.com/stevenguy/bc-project-3/tree/mearat'>
-                            <Icon name='code branch' />
-                            Github Link
-                        </a>
-                    </Card.Content>
-                </Card>
-            </div>
-            <div> 
-                <Card className='cards' fluid>
-                    <Image src='./images/deezpolls.webp' />
-                    <Card.Content>
-                    <Card.Header>Deez Polls</Card.Header>
-                    <Card.Meta>Customized Polling Web Application</Card.Meta>
-                    <Card.Description>2nd Group Project Express application using Sequelize and deployed via Heroku</Card.Description>
-                    </Card.Content>
-                    <Card.Content className='cardsLink' extra>
-                        <a target='blank' href='https://afternoon-crag-56596.herokuapp.com/polls'>
-                            <Icon name='globe' />
-                            Demo Link
-                        </a>
-                        <a target='blank' href='https://github.com/bunrithbuth/bc-project-2/tree/master'>
-                            <Icon name='code branch' />
-                            Github Link
-                        </a>
-                    </Card.Content>
-                </Card>
-            </div>
-            <div> 
-                <Card className='cards' fluid>
-                    <Image src='./images/triviagame.webp' />
-                    <Card.Content>
-                    <Card.Header>Trivia Game</Card.Header>
-                    <Card.Meta>Basic Javascript App</Card.Meta>
-                    <Card.Description>Javascript functions and methods warm up in a form of U.S citizenship trivia game.</Card.Description>
-                    </Card.Content>
-                    <Card.Content className='cardsLink' extra>
-                        <a target='blank' href='https://mearatjames.github.io/TriviaGame/'>
-                            <Icon name='globe' />
-                            Demo Link
-                        </a>
-                        <a target='blank' href='https://github.com/mearatjames/TriviaGame'>
-                            <Icon name='code branch' />
-                            Github Link
-                        </a>
-                    </Card.Content>
-                </Card>
-            </div>
+            {portfolioContents.map((e, i) => (
+                <div key={i}> 
+                    <Card className='cards' fluid>
+                        <Image src={e.images} />
+                        <Card.Content>
+                        <Card.Header>{e.header}</Card.Header>
+                        <Card.Meta>{e.meta}</Card.Meta>
+                        <Card.Description>{e.description}</Card.Description>
+                        </Card.Content>
+                        <Card.Content className='cardsLink' extra>
+                            <a target='blank' href={e.demoLink}>
+                                <Icon name='globe' />
+                                Demo Link
+                            </a>
+                            <a target='blank' href={e.githubLink}>
+                                <Icon name='code branch' />
+                                Github Link
+                            </a>
+                        </Card.Content>
+                    </Card>
+                </div>
+            ))}
          </Container>
         </div>
     </div>
